82824fc048c9e3edaf302e75fa6b736e5feb5f11
dyeray <dyeray@gmail.com>
2021-10-04 19:14:00 +0100

    Update code to stop using innerHTML. Fix preventdefault usage.

--- addon/ui/options.js
+++ addon/ui/options.js
@@ -2,19 +2,21 @@
 
 import { getCodes, storeCodes } from "/utils/storage.js";
 
-async function loadCurrentOptions() {
+async function loadCurrentOptions(e) {
     document.querySelector("form").reset();
     let codes = await getCodes();
-    let innerHTML = codes.map(item => getCodeRow(item)).join("");
-    document.querySelector("#currentCodes").innerHTML = innerHTML;
+    let table = buildTable(codes);
+    let codesNode = document.querySelector("#currentCodes");
+    codesNode.textContent = "";
+    codesNode.appendChild(table);
 }
 
 async function saveOptions(e) {
+    e.preventDefault();
     let codes = await getCodes()
     let newCode = {domain: document.querySelector("#domain").value, codeName: document.querySelector("#codeName").value};
     await storeCodes([...codes, newCode]);
     await loadCurrentOptions();
-    e.preventDefault();
 }
 
 async function removeCode(e) {
@@ -28,8 +30,29 @@ async function removeCode(e) {
     await loadCurrentOptions();
 }
 
-function getCodeRow(codeSetting) {
-    return `<tr><td>${codeSetting.domain}</td><td>${codeSetting.codeName}</td><td class="removeCode" data-codename="${codeSetting.codeName}">❌</td></tr>`
+function buildTable(codes) {
+    let table = createElement("table");
+    codes.map(item => { table.appendChild(buildRow(item)) });
+    return table;
+}
+
+function buildRow(codeSetting) {
+    let row = createElement("tr");
+    row.appendChild(createElement("td", `${codeSetting.domain}`));
+    row.appendChild(createElement("td", `${codeSetting.codeName}`));
+    row.appendChild(createElement("td", `❌`, {"class": "removeCode", "data-codename": `${codeSetting.codeName}`}));
+    return row;
+}
+
+function createElement(tagName, text = null, attributes = {}) {
+    let element = document.createElement(tagName);
+    if (text != null) {
+        element.textContent = text;
+    }
+    for (const [key, value] of Object.entries(attributes)) {
+        element.setAttribute(key, value);
+    }
+    return element;
 }
 
 document.addEventListener("DOMContentLoaded", loadCurrentOptions);
